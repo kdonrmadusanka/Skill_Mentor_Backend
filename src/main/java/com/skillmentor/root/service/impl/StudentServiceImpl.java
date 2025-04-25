20c5afe9c05d01adaa328558f0cda08ef7906266
kdonrmadusanka <ravmkdon@gmail.com>
2025-04-25 22:33:16 +0530

    Implement the StudentServiceImpl with StudentService interface.

--- src/main/java/com/skillmentor/root/service/impl/StudentServiceImpl.java
+++ src/main/java/com/skillmentor/root/service/impl/StudentServiceImpl.java
@@ -2,10 +2,11 @@ package com.skillmentor.root.service.impl;
 
 import com.skillmentor.root.dto.StudentDTO;
 import com.skillmentor.root.repository.StudentRepository;
+import com.skillmentor.root.service.StudentService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentServiceImpl {
+public class StudentServiceImpl implements StudentService {
 
     StudentRepository studentRepository;
 
@@ -13,9 +14,9 @@ public class StudentServiceImpl {
         this.studentRepository = studentRepository;
     }
 
-    public StudentDTO createStudent(StudentDTO studentDTO){
+    @Override
+    public StudentDTO createStudent(StudentDTO studentDTO) {
         StudentDTO newStudent = studentRepository.createStudent(studentDTO);
         return newStudent;
     }
-
 }

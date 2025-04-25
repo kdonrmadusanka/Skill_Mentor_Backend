3e25f241c13baca4b4d939140a5189062560816f
kdonrmadusanka <ravmkdon@gmail.com>
2025-04-25 18:43:36 +0530

    Create student data transfer object.

---
+++ src/main/java/com/skillmentor/root/StudentDTO/StudentDTO.java
@@ -0,0 +1,78 @@
+package com.skillmentor.root.StudentDTO;
+
+public class StudentDTO {
+
+    private String studentId;
+    private String firstName;
+    private String lastname;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private Integer age;
+
+    public StudentDTO(String studentId, String firstName, String lastname, String email, String phoneNumber, String address, Integer age) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}

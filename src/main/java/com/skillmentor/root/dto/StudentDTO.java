178e079866f31ced213865de0ebfbd8ae4742e09
kdonrmadusanka <ravmkdon@gmail.com>
2025-05-25 11:02:00 +0530

    feat: add StudentDTO with validation and JSON property annotations
    - Introduced StudentDTO with fields: studentId, firstName, lastName, email, phoneNumber, address, and age
    - Applied validation annotations to enforce input constraints (e.g., @NotBlank, @Min)
    - Used @JsonProperty to standardize JSON field names
    - Included @JsonInclude to exclude null values from serialized output

--- src/main/java/com/skillmentor/root/dto/StudentDTO.java
+++ src/main/java/com/skillmentor/root/dto/StudentDTO.java
@@ -1,89 +1,39 @@
 package com.skillmentor.root.dto;
 
-public class StudentDTO {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    private Integer objectId;
-    private String studentId;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StudentDTO {
+    @JsonProperty("student_id")
+    private Integer studentId;
+    @NotBlank(message = "First name must not be blank")
+    @JsonProperty("first_name")
     private String firstName;
+    @NotBlank(message = "Last name must not be blank")
+    @JsonProperty("last_name")
     private String lastName;
+    @NotBlank(message = "Email must not be blank")
+    @JsonProperty("email")
     private String email;
+    @NotBlank(message = "Phone number must not be blank")
+    @JsonProperty("phone_number")
     private String phoneNumber;
+    @NotBlank(message = "Address must not be blank")
+    @JsonProperty("address")
     private String address;
+    @NotNull(message = "Age must not be null")
+    @Min(value = 18, message = "Age must be at least 18")
+    @JsonProperty("age")
     private Integer age;
 
-    public StudentDTO(String studentId, String firstName, String lastname, String email, String phoneNumber, String address, Integer age) {
-        this.studentId = studentId;
-        this.firstName = firstName;
-        this.lastName = lastname;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.age = age;
-    }
-
-    public StudentDTO(){};
-
-    public Integer getObjectId() {
-        return objectId;
-    }
-
-    public void setObjectId(Integer objectId) {
-        this.objectId = objectId;
-    }
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastName = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }

1fa7a24bd984bd2384e13eec47bec75b0e33076f
kdonrmadusanka <ravmkdon@gmail.com>
2025-05-25 10:58:50 +0530

    feat: add ClassRoomDTO with validation and JSON property mapping
    - Added fields: classRoomId, title, enrolledStudentCount, and mentorDTO
    - Applied validation annotations for title and enrolledStudentCount
    - Used @JsonProperty for consistent JSON field naming
    - Included @JsonInclude to exclude null fields from JSON output

--- src/main/java/com/skillmentor/root/dto/ClassRoomDTO.java
+++ src/main/java/com/skillmentor/root/dto/ClassRoomDTO.java
@@ -1,72 +1,29 @@
 package com.skillmentor.root.dto;
 
-import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ClassRoomDTO {
 
-    private Integer objectId;
-    private String classRoomId;
+    @JsonProperty("class_room_id")
+    private Integer classRoomId;
+    @NotBlank(message = "Title must not be blank")
+    @JsonProperty("title")
     private String title;
-    private Double sessionFee;
+    @NotNull(message = "Enrolled student count must not be null")
+    @JsonProperty("enrolled_student_count")
     private Integer enrolledStudentCount;
-    private List<MentorDTO> mentorDTO;
+    @JsonProperty("mentor")
+    private MentorDTO mentorDTO;
 
-    public ClassRoomDTO(){};
-
-    public ClassRoomDTO(Integer objectId, String classRoomId, String title, Integer enrolledStudentCount, Double sessionFee, List<MentorDTO> mentorDTO) {
-        this.objectId = objectId;
-        this.classRoomId = classRoomId;
-        this.title = title;
-        this.enrolledStudentCount = enrolledStudentCount;
-        this.sessionFee = sessionFee;
-        this.mentorDTO = mentorDTO;
-    }
-
-    public Integer getObjectId() {
-        return objectId;
-    }
-
-    public void setObjectId(Integer objectId) {
-        this.objectId = objectId;
-    }
-
-    public String getClassRoomId() {
-        return classRoomId;
-    }
-
-    public void setClassRoomId(String classRoomId) {
-        this.classRoomId = classRoomId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Double getSessionFee() {
-        return sessionFee;
-    }
-
-    public void setSessionFee(Double sessionFee) {
-        this.sessionFee = sessionFee;
-    }
-
-    public Integer getEnrolledStudentCount() {
-        return enrolledStudentCount;
-    }
-
-    public void setEnrolledStudentCount(Integer enrolledStudentCount) {
-        this.enrolledStudentCount = enrolledStudentCount;
-    }
-
-    public List<MentorDTO> getMentorDTO() {
-        return mentorDTO;
-    }
-
-    public void setMentorDTO(List<MentorDTO> mentorDTO) {
-        this.mentorDTO = mentorDTO;
-    }
 }
